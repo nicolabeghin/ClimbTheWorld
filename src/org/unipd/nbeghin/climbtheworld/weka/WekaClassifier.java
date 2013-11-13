07cb8fbefaf5998f918a26e9fcce96db818aaf41
Nicola Beghin <nicolabeghin@gmail.com>
2013-11-13 23:39:10 +0100

    new classifier: many samples and high precision

--- src/org/unipd/nbeghin/climbtheworld/weka/WekaClassifier.java
+++ src/org/unipd/nbeghin/climbtheworld/weka/WekaClassifier.java
@@ -19,41 +19,17 @@ public class WekaClassifier {
 
 	public static double classify(Object[] i) throws Exception {
 		double p = Double.NaN;
-		p = WekaClassifier.N10e9c5920(i);
+		p = WekaClassifier.N1393537d0(i);
 		return p;
 	}
 
-	static double N10e9c5920(Object[] i) {
+	static double N1393537d0(Object[] i) {
 		double p = Double.NaN;
 		if (i[1] == null) {
 			p = 1;
 		} else if (((Double) i[1]).doubleValue() <= 0.1) {
 			p = 1;
 		} else if (((Double) i[1]).doubleValue() > 0.1) {
-			p = WekaClassifier.N63a9fcea1(i);
-		}
-		return p;
-	}
-
-	static double N63a9fcea1(Object[] i) {
-		double p = Double.NaN;
-		if (i[5] == null) {
-			p = 0;
-		} else if (((Double) i[5]).doubleValue() <= 0.01) {
-			p = WekaClassifier.N2d1b99c42(i);
-		} else if (((Double) i[5]).doubleValue() > 0.01) {
-			p = 0;
-		}
-		return p;
-	}
-
-	static double N2d1b99c42(Object[] i) {
-		double p = Double.NaN;
-		if (i[6] == null) {
-			p = 1;
-		} else if (((Double) i[6]).doubleValue() <= 0.21) {
-			p = 1;
-		} else if (((Double) i[6]).doubleValue() > 0.21) {
 			p = 0;
 		}
 		return p;
